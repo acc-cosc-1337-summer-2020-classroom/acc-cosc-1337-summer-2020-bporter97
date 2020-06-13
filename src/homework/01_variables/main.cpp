f6a8c4e2c26e2e11cc3110baff11181da595559a
Blake Porter <bporter78745@gmail.com>
2020-06-12 20:35:22 -0500

    Homework Assignment 1

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,5 +1,9 @@
 ﻿//write include statements
 
+#include "variables.h"
+
+
+using std::cout; using std::cin; using std::setw; using std::fixed;
 
 //write namespace using statement for cout
 
@@ -9,7 +13,28 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 */
 int main()
 {
-	
+	double meal_amount{0};
+	double tip_rate{0};
+	double tip_amount{0};
+	double tax_amount{0};
+	double total{0};
+
+	cout<<"Please Enter the cost of your meal: ";
+	cin>>meal_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Please Enter the rate you wish to tip your server: ";
+	cin>> tip_rate;
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<fixed<<std::setprecision(2);
+	cout<<setw(17)<<"Meal Amount: "<<setw(7)<<meal_amount<<"\n";
+	cout<<setw(17)<<"Sales Tax: "<<setw(7)<<tax_amount<<"\n";
+	cout<<setw(17)<<"Tip Amount: "<<setw(7)<<tip_amount<<"\n";
+	cout<<setw(17)<<"Total: "<<setw(7)<<total<<"\n";
+
 
 	return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -1,13 +1,34 @@
 #include "variables.h"
 
 //example
-int add_numbers(int num1, int num2)
+/* int add_numbers(int num1, int num2)
 {
 	return num1 + num2;
 }
+*/
 
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
+
+double get_sales_tax_amount(double meal_amount)
+{
+	double tax_rate{.0675};
+	double tax_payed{0};
+
+	tax_payed = meal_amount * tax_rate;
+	return tax_payed;
+
+}
+
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	double tip_amount{0};
+
+	tip_amount = meal_amount * tip_rate;
+	return tip_amount;
+
+}
+
 
 
 
