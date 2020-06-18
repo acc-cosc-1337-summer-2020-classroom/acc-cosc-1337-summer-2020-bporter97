bd893b4868a95ce33842bbeaaee94f6317ea2b08
Blake Porter <bporter78745@gmail.com>
2020-06-18 18:31:28 -0500

    class

--- src/examples/04_module/04_for_ranged/main.cpp
+++ src/examples/04_module/04_for_ranged/main.cpp
@@ -2,12 +2,49 @@
 #include<iostream>
 #include "for_ranged.h"
 
+using std::cin;
+
 int main() 
 {
-	std::string name = "John";
+	char letter = 'a';
+	char letter2 = 'b';
+	letter2 == letter;
+	std::string letter_string = "a";
+	//Creating strings
+	int num = 5;
+	std::string name1;
+	std::string name = "joe"; //create string assign joe to it
+	std::string name2{"John"};
+	std::string name3(9, 'a');
+
+	//string operation
+	if(name1 == name)
+	{
+		std::cout<<"same";
+	}
+	
+	name1 += name; //0=j, 1=0, 2=e
+	name1[1];
+	name.push_back('y'); //name is now joey
+	name2.insert(3, "y"); //john would be johny
+	std::cout<<name2;
+
+	for(auto n: name2)
+	{
+		n = 'a';
+		//std::cout<<n<<"\n";
+	}
+
+	std::string last_name;
+	std::cout<<"Enter last name: ";
+	std::getline(cin, last_name);
+	std::cout<<last_name;
+	
+	/*std::string name = "John";
 	loop_string_w_index(name);
 	std::cout<<"\n\n";
 	loop_string_for_range(name);
+	*/
 
 	return 0;
 }
--- src/examples/04_module/06_vectors/main.cpp
+++ src/examples/04_module/06_vectors/main.cpp
@@ -1,7 +1,34 @@
 #include "vec.h"
+#include<vector>
+
+using std::vector;
+using std::string;
+using 
 
 int main() 
 {
+	vector<int> num; //empty vector
+	vector<int> num1(5);
+
+	for(auto n: num1)
+	{
+		std::cout<<n<<"\n";
+	}
+
+	vector<char> char(5, 'z');
+
+	for(auto c: char1)
+	{
+		std::cout<<c<<"\n";
+	}
+
+	vector<double> dubs{1.5, 2.5};
+	vector<std::string> names {"joe", "john", "mary"}:
+	names.push_back("Jill");
+	for(auto n: names)
+	{
+		std::cout<<n<<"\n";
+	}
 
 	return 0;
 }
