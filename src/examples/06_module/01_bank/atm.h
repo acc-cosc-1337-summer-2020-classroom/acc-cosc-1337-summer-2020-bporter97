e6a5f700dd6fa7889a182c4466c1e4e04889005c
Blake Porter <bporter78745@gmail.com>
2020-06-25 18:16:29 -0500

    add reference bankaccount

--- src/examples/06_module/01_bank/atm.h
+++ src/examples/06_module/01_bank/atm.h
@@ -1,1 +1,21 @@
+  
 //atm.h
+#include "bank_account.h"
+#ifndef ATM_H
+#define ATM_H
+
+class ATM
+{
+public:
+    ATM(BankAccount& a) : account{a}{}
+    void run();
+private:
+    void display_menu();//helper functions..utility functions
+    void set_choice();
+    void handle_transaction();
+    BankAccount& account;//our user-defined class--composition
+    int choice;
+
+};
+
+#endif
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,34 +1,32 @@
+  
+#include "atm.h"
 #include "bank_account.h"
 #include<iostream>
 
-using namespace::std; 
+using std::cout;
+using std::cin;
 
 int main()
 {
-	BankAccount account = get_account(1);
-	cout<<account.get_bank_balance()<<"\n";
+	BankAccount& account = get_account(1);
+	cout<<"Account: "<<account.get_balance()<<"\n";
+	//ATM atm(account);
+	//atm.run();
 
+	BankAccount account1 = account;
+	cout<<"Account1: "<<account1.get_balance()<<"\n";
 	
+	account.deposit(50);
+	cout<<"Account: "<<account.get_balance()<<"\n";
 	
-	/*
-	BankAccount account(1000); //create an instance/variable of class in stack memory
+	cout<<"Account1: "<<account1.get_balance()<<"\n";
 
-	int balance = account.get_balance();
-	cout<<"balance: " <<balance<<"\n";
-
-	
-	int deposit;
-	cout<<"Enter deposit amount: ";
-	cin>>deposit;
-	account.deposit(deposit);
-	cout<<"Balance: "<<account.get_balance()<<"\n";
-
-	int withdraw;
-	cout<<"Enter withdraw amount: ";
-	cin>>withdraw;
-	account.withdraw(withdraw);
-	cout<<"Balance: "<<account.get_balance()<<"\n";
-	*/
+	for(int i=0; i < 3;++i)
+	{
+		BankAccount a = get_account(i);
+		cout<<a.get_balance()<<"\n";
+	}
 
 	return 0;
+
 }
