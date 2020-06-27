c99b8f23ebdd7ca6ed39f0584dff43adf6b3cc44
Blake Porter <bporter78745@gmail.com>
2020-06-26 23:07:59 -0500

    Homework_03

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -7,14 +7,37 @@ Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
 
+double get_gc_content(const std::string &dna)
+{
+    double gc = 0;
 
+    for(int i = 0; i < dna.length(); i++)
+    {
+        if(dna[i] == 'G' || dna[i] == 'C')
+        {
+            gc++;
+        }
+    }
 
-
+    double gc_content = (gc / dna.length());
+    return gc_content;
+}
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+std::string get_reverse_string(std::string dna)
+{
+    std::string rev;
 
+    for(int i = dna.size() -1; i >= 0; i--)
+    {
+        rev = rev.append(1, dna[i]);
+    }
+
+
+    return rev;
+}
 
 
 /*
@@ -27,4 +50,32 @@ b. iterate local string variable and
 c. return string
 
 */
+std::string get_dna_complement(std::string dna)
+{
+    std::string dna_reverse_complement = get_reverse_string(dna);
 
+    for(int i = 0; i<dna.length(); i++)
+    {
+        if(dna[i] == 'A')
+        {
+            dna_reverse_complement[i] = 'T';
+        }
+        else if(dna[i] == 'T')
+        {
+            dna_reverse_complement[i] = 'A';
+        }
+        else if(dna[i] == 'G')
+        {
+            dna_reverse_complement[i] = 'C';
+        }
+        else
+        {
+            dna_reverse_complement[i] = 'G';
+        }
+        
+
+    }
+
+    return dna_reverse_complement;
+
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
-
+#include "dna.h"
+#include<iostream>
 //write using statements
-
+using namespace std;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,38 @@ user enters a y or Y.
 */
 int main() 
 {
+	int choice;
+	string dna;
+	do
+	{
+	
+		cout<<"Please enter 1 for Get GC content, or 2 for Get DNA Complement: Press 'y' or 'Y' to quit \n";
+		cin>>choice;
+		cout<<"Please enter the DNA string: \n";
+		cin>>dna; 
+	
+		if(choice == 1)
+		{
+			get_gc_content(dna);
+			cout<<get_gc_content(dna);
+		}
+		else if(choice == 2)
+		{
+			get_dna_complement(dna);
+			cout<<get_dna_complement(dna);
+		}
+		else
+		{
+			cout<<"That is an invalid choice";
+			return 0;
+		}
+		
+		
+		
+	} while (choice != 'y' || choice != 'Y');
+	
+
+
+
 	return 0;
 }
